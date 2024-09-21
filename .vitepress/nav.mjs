8359af1a7e5f493e8a97a6b364ec6dff101cb80e
chenml <835967371@qq.com>
2024-09-21 15:04:36 +0800

    tj

--- .vitepress/nav.mjs
+++ .vitepress/nav.mjs
@@ -2,5 +2,12 @@ import qianduantixiNav from "./前端体系/nav.mjs";
 import shizhanNav from "./实战/nav.mjs";
 import jinjieNav from "./进阶/nav.mjs";
 import yuedubijiNav from "./阅读笔记/nav.mjs";
+import shenghuozhinanNav from "./生活指南/nav.mjs";
 
-export default [qianduantixiNav, shizhanNav, jinjieNav, yuedubijiNav];
+export default [
+  qianduantixiNav,
+  shizhanNav,
+  jinjieNav,
+  yuedubijiNav,
+  shenghuozhinanNav,
+];
