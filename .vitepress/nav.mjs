648e2c0d32281bc8f27d73e9d6a3ffe345fae8a5
陈明灵 <5749225+ming_ling_chen@user.noreply.gitee.com>
2025-05-05 22:32:47 +0800

    提交

--- .vitepress/nav.mjs
+++ .vitepress/nav.mjs
@@ -1,13 +1,6 @@
 import qianduantixiNav from "./前端体系/nav.mjs";
 import shizhanNav from "./实战/nav.mjs";
 import jinjieNav from "./进阶/nav.mjs";
-import yuedubijiNav from "./阅读笔记/nav.mjs";
 import daohangNav from "./导航/nav.mjs";
 
-export default [
-  qianduantixiNav,
-  shizhanNav,
-  jinjieNav,
-  yuedubijiNav,
-  daohangNav,
-];
+export default [qianduantixiNav, shizhanNav, jinjieNav, daohangNav];
--- .vitepress/阅读笔记/nav.mjs
+++
@@ -1,4 +0,0 @@
-export default {
-  text: "阅读笔记",
-  items: [{ text: "技术管理", link: "/阅读笔记/技术管理/00 技术管理实战36讲" }],
-};
