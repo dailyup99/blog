a9511af8499fc081c4de85960cef12a4cfe0b0e5
陈明灵 <5749225+ming_ling_chen@user.noreply.gitee.com>
2025-06-15 00:50:32 +0800

    1

--- .vitepress/nav.mjs
+++ .vitepress/nav.mjs
@@ -1,5 +1,11 @@
 import qianduantixiNav from "./前端体系/nav.mjs";
 import jinjieshizhanNav from "./进阶实战/nav.mjs";
+import qianduanjiagouNav from "./前端架构/nav.mjs";
 import daohangNav from "./导航/nav.mjs";
 
-export default [qianduantixiNav, jinjieshizhanNav, daohangNav];
+export default [
+  qianduantixiNav,
+  jinjieshizhanNav,
+  qianduanjiagouNav,
+  daohangNav,
+];
--- .vitepress/sidebar.mjs
+++ .vitepress/sidebar.mjs
@@ -1,7 +1,9 @@
 import qianduantixiSidebar from "./前端体系/sidebar.mjs";
 import jinjieshizhanSidebar from "./进阶实战/sidebar.mjs";
+import qianduanjiagouSidebar from "./前端架构/sidebar.mjs";
 
 export default {
   ...qianduantixiSidebar,
   ...jinjieshizhanSidebar,
+  ...qianduanjiagouSidebar,
 };
