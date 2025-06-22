faadc5965ec060e99cb7ffd16c5919857baaeea5
陈明灵 <5749225+ming_ling_chen@user.noreply.gitee.com>
2025-06-23 00:48:09 +0800

    1

--- .vitepress/nav.mjs
+++ .vitepress/nav.mjs
@@ -1,11 +1,5 @@
 import qianduantixiNav from "./前端体系/nav.mjs";
 import jinjieshizhanNav from "./进阶实战/nav.mjs";
-import qianduanjiagouNav from "./前端架构/nav.mjs";
 import daohangNav from "./导航/nav.mjs";
 
-export default [
-  qianduantixiNav,
-  jinjieshizhanNav,
-  qianduanjiagouNav,
-  daohangNav,
-];
+export default [qianduantixiNav, jinjieshizhanNav, daohangNav];
