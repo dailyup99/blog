2a6f21baccae4f6504aa06b45288c82730820610
陈明灵 <835967371@qq.com>
2026-07-16 14:47:11 +0800

    1

--- .vitepress/nav.mjs
+++ .vitepress/nav.mjs
@@ -1,6 +1,5 @@
 import qianduantixiNav from "./前端体系/nav.mjs";
 import jinjieshizhanNav from "./进阶实战/nav.mjs";
 import daohangNav from "./导航/nav.mjs";
-import ziliaoNav from "./资料/nav.mjs";
 
-export default [qianduantixiNav, jinjieshizhanNav, ziliaoNav, daohangNav];
+export default [qianduantixiNav, jinjieshizhanNav, daohangNav];
